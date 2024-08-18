5ad5aa325a1644d183518ce3758c060278714474
divakar bhardwaj <dbhardwaj@contecglobal.com>
2024-08-18 12:52:00 +0530

    Add project files.

---
+++ Controllers/OrderMessageController.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderMessageController : ControllerBase
+    {
+        private readonly string kafkaTopic = "glo-webhook-topic";
+        private readonly string kafkaBootstrapServers = "192.168.0.136:9092"; // Update with your Kafka broker address
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] OrderMessage message)
+        {
+            var config = new ProducerConfig { BootstrapServers = kafkaBootstrapServers };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var jsonMessage = JsonSerializer.Serialize(message);
+                var result = await producer.ProduceAsync(kafkaTopic, new Message<Null, string> { Value = jsonMessage });
+
+                return Ok(new { result.TopicPartitionOffset });
+            }
+        }
+    }
+}
---
+++ Models/OrderMessage.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplication1.Models
+{
+    public class OrderMessage
+    {
+        public string OrderMsgId { get; set; }
+        public string From { get; set; }
+        public string PartnerID { get; set; }
+        public string ProductType { get; set; }
+        public string ProductID { get; set; }
+        public string ServiceList { get; set; }
+        public string UpdateType { get; set; }
+        public string UpdateTime { get; set; }
+        public string EffectiveTime { get; set; }
+        public string ExpiryTime { get; set; }
+        public string ChannelID { get; set; }
+        public string ShortMessage { get; set; }
+        public string Status { get; set; }
+        public string IsFree { get; set; }
+        public string RateCode { get; set; }
+        public string ProductPeriodType { get; set; }
+        public string ActualValidate { get; set; }
+        public string NextChargingTime { get; set; }
+    }
+
+}
